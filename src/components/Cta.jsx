0cc3fb2414abb3dd38345720b8bee7ba399a1c96
abdalrahmanhajjo <abedhajjo57@gmail.com>
2026-06-04 03:03:14 +0300

    last update

--- src/components/Cta.jsx
+++ src/components/Cta.jsx
@@ -6,7 +6,15 @@ export default function Cta() {
       id="contact"
       className="cta-section text-white overflow-hidden position-relative"
     >
-      <div className="cta-notch"></div>
+      <div className="cta-notch">
+        {/* SVG stretches to any viewport width and gives rounded V-notch corners */}
+        <svg className="cta-notch-svg" preserveAspectRatio="none" viewBox="0 0 1000 68" aria-hidden="true">
+          <path
+            d="M 0 0 L 120 0 L 137.7 60.3 Q 140 68 148 68 L 382 68 Q 390 68 392.3 60.3 L 410 0 L 1000 0 Z"
+            fill="white"
+          />
+        </svg>
+      </div>
 
       <img
         className="cta-image"
